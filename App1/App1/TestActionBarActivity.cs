a72547f5b36dddb408889f47dfaf2ea417172b73
Robert Castles <rcastles@pmg.net>
2014-10-27 14:22:30 -0400

    reinitialized files

---
+++ App1/App1/TestActionBarActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/ApplicationTheme")]
+    public class TestActionBarActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.TestActionBarActivity);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            ActionBar actionBar = this.ActionBar;
+            actionBar.Show();
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.test_actionbar_menu, menu);
+            //var menuItem = menu.FindItem(Resource.Id.menu_settings);
+
+            return base.OnCreateOptionsMenu(menu);
+        }
+    }
+}
+
